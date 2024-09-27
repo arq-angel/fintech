43f3fae0373cba05c20256eec90d0e54414aa10e
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-09-27 23:20:01 +1000

    complete app setup and expo prebuild

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Index = () => {
+    return (
+        <View>
+            <Text>Page</Text>
+        </View>
+    );
+};
+
+export default Index;
