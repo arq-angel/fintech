a0f06feec8cb6a407e2dfcb1dac3d59b34f704a8
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-09-28 15:30:18 +1000

    complete background video and signup screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,54 +1,108 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {useFonts} from 'expo-font';
+import {Link, Stack, useRouter} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import {useColorScheme} from '@/components/useColorScheme';
+
+import Colors from "@/constants/Colors";
+import {TouchableOpacity} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
+import {StatusBar} from "expo-status-bar";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
 } from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+const InitialLayout = () => {
+    const [loaded, error] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        ...FontAwesome.font,
+    });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    const router = useRouter();
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
+    return (
+        <Stack>
+            <Stack.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                }}/>
+            <Stack.Screen
+                name="signup"
+                options={{
+                    title: '',
+                    headerBackTitle: '',
+                    headerShadowVisible: false,
+                    headerStyle: {backgroundColor: Colors.background},
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={router.back}>
+                            <Ionicons name="arrow-back" size={34} color={Colors.dark}/>
+                        </TouchableOpacity>
+                    )
+                }}/>
+            <Stack.Screen
+                name="login"
+                options={{
+                    title: '',
+                    headerBackTitle: '',
+                    headerShadowVisible: false,
+                    headerStyle: {backgroundColor: Colors.background},
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={router.back}>
+                            <Ionicons name="arrow-back" size={34} color={Colors.dark}/>
+                        </TouchableOpacity>
+                    ),
+                    headerRight: () => (
+                        <Link href={'/help'} asChild>
+                            <TouchableOpacity>
+                                <Ionicons name="help-circle" size={34} color={Colors.dark}/>
+                            </TouchableOpacity>
+                        </Link>
+                    )
+                }}/>
+            <Stack.Screen
+                name="help"
+                options={{
+                    title: 'Help',
+                    presentation: 'modal'
+                }}
+                />
+        </Stack>
+    );
 }
 
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+const RootLayoutNav = () => {
+    return (
+        <GestureHandlerRootView style={{flex: 1}}>
+            <StatusBar style="light"/>
+            <InitialLayout/>
+        </GestureHandlerRootView>
 
-  return (
-      <Stack>
-      </Stack>
-  );
+    );
 }
+
+export default RootLayoutNav;
